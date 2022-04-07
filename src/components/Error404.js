e954cb31e064851cd9c214c8cccebe0ba93ed399
JavierSantoyo89 <javier.santoyo@yahoo.com>
2022-04-07 17:48:08 -0500

    Downgrade React 17

---
+++ src/components/Error404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error404(props){
+    return (
+        <header className='hearderDiv'>
+            <img src='../assets/img/404Backfround.png' alt='Error 404'/>
+        </header>
+    );
+}
+
+export default Error404;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard(props){
+    return(
+        <div>
+            Soy el dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
