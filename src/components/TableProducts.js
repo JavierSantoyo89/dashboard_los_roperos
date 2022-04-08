7c463390de010ab888d5796c99278220182c7863
JavierSantoyo89 <javier.santoyo@yahoo.com>
2022-04-07 20:59:18 -0500

    Workimg Table products

---
+++ src/components/TableProducts.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles'
+
+
+const StyledTableCell = withStyles(()=>({
+    head:{
+        textAlign: 'center',
+        background: 'black',
+        color: 'white'
+    },
+    body:{
+        fontSize:14
+    },
+}))(TableCell)
+
+
+
+function TableProducts(){
+
+    
+
+// Seccion de todos los productos
+const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        
+            fetch('http://localhost:1689/products/all')
+                .then(response => response.json())
+                .then(data =>{
+                    setProducts(data.data)
+                    
+                })
+                .catch(error => console.error(error))
+            console.log("%c Se monto el componente", "color: green");
+           // console.log(response.data);
+           console.log(fetch);
+           
+    
+    },[])
+
+    useEffect(()=>{ 
+            console.log("%c Se actualizo el componente", "color: yellow");
+            console.log(fetch.statusText);
+    },[products])
+
+    useEffect(()=>{
+        return()=>{
+            console.log("%c Se desmonto el componente", "color: red");
+        }
+    },[]);
+
+// fin de seccion de todos los productos
+    return(
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Producto</StyledTableCell>
+                        <StyledTableCell>ID</StyledTableCell>
+                        <StyledTableCell>Nombre</StyledTableCell>
+                        <StyledTableCell>Precio</StyledTableCell>
+                        <StyledTableCell>Accion</StyledTableCell>
+
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {products.map(product=>(
+                        <TableRow key = {product.id}>
+                            <TableCell>
+                                <img src={'http://localhost:1689/images/products/' + product.imgProduct} alt ='avatar' width='65px' height='85px'/>    
+                            </TableCell>
+                            <TableCell> 
+                                {product.id}
+                            </TableCell>
+                            <TableCell> 
+                                {product.name}
+                            </TableCell>
+                            <TableCell> 
+                                {product.price}
+                            </TableCell>
+                        </TableRow>
+                                
+                    ))}
+
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default TableProducts
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,20 +8,16 @@ import CardsHeaderCountProducts from '../components/CardsHeaderCountProducts'
 import HouseIcon from '@material-ui/icons/House';
 import CardsHeaderCountUsers from '../components/CardsHeaderCountUsers';
 import CardsHeaderCountSaleSeason from '../components/CardsHeaderCountSaleSeason'
-
-
-
-
-
-
-
-
+import TableProducts from '../components/TableProducts'
 const useStyles = makeStyles(()=>({
     root:{
         flexGrow: 1
     },
     iconos:{
         color: 'white'
+    },
+    containerTabla:{
+        marginTop: '40px'
     }
 }))
 
@@ -47,13 +43,11 @@ function Dashboard(props){
 
             </Grid>
 
-            <Grid container spacing={5} className = {classes.container}>
-
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-            <CardsHeaderCountProducts icono={<HouseIcon className={classes.iconos}/>} titulo='In-sale/Season' texto='In-sale' color='rgba(248,80,50,1)' font= 'white'/>
-                    
-            </Grid>
-            </Grid>
+        
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={classes.containerTabla}>
+                    <TableProducts />
+                </Grid>
+         
         </div>
     )
 }
